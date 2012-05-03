11c038f795540ffe51c6e08de90cb398da808514
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2012-05-03 21:54:25 +0200

    basic cli with types Ok

---
+++ bin/cli.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+cube2csv = require('../lib/cube2csv');
+
+var argv = require('optimist')
+        .usage('Usage: $0 [metric, event, types] {OPTIONS}')
+        .wrap(80)
+        .option('expression', {
+          alias : 'e',
+          desc : 'Expression to be evaluated by Cube'
+        })
+        .option('host', {
+          alias : 'H',
+          desc : 'Cube evaluator host',
+          default: 'localhost:8081'
+        })
+        .option('request', {
+          alias : 'r',
+          desc : 'Ouput only the computed query',
+          default : false,
+          boolean : true
+        })
+        .option('help', {
+          alias : 'h',
+          desc : 'Display this message'
+        })
+        .check(function(argv) {
+          if(argv.help) throw '';
+          if(['types', 'metric', 'event'].indexOf(argv._[0] ) === -1)
+            throw 'Specify \'types\', \'metric\' or \'event\'' ;
+        })
+        .argv;
+
+switch(argv._[0]) {
+  case 'types':
+    if(argv.request)
+      console.log(cube2csv.buildTypesQuery(argv.host));
+    else
+      cube2csv.types(argv.host, console.log);
+    break;
+
+  case 'event':
+    break;
+    
+  case 'metric':
+    break;
+}
